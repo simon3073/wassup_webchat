50bf1a7d7d65501d38d6dd694eb81652401f16ff
simon3073 <simonbertoli@gmail.com>
2022-08-08 09:00:23 +1000

    added dotenv and db as environmental vars

--- database/firebaseDB.js
+++ database/firebaseDB.js
@@ -3,14 +3,16 @@ import firebase from 'firebase/compat/app'
 import auth from 'firebase/compat/auth'
 import firestore from 'firebase/compat/firestore'
 
+import { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId, measurementId } from '@env'
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyAhhT4upQifOVeqmcXtEsTCCBsHQ91TVVo',
-  authDomain: 'wassup-web-chat.firebaseapp.com',
-  projectId: 'wassup-web-chat',
-  storageBucket: 'wassup-web-chat.appspot.com',
-  messagingSenderId: '1006250612990',
-  appId: '1:1006250612990:web:dd5a0c28e0bd457b26edaf',
-  measurementId: 'G-895Z0VKQWV',
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId,
 }
 
 firebase.initializeApp(firebaseConfig)
